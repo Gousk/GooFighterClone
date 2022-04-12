e5574d139f7011f5e5a5b17ff2f8f33b0b2dd8c1
Önder Balta <onderman70@gmail.com>
2022-04-12 03:06:38 +0300

    asd

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -29,7 +29,7 @@ public class Bullet : MonoBehaviour
     {
         Vector3 minScale = new Vector3(5F,5F,1F);
         Vector3 minusScale = new Vector3(0.5F,0.5F,0.2F);
-        Vector3 bossMinScale = new Vector3(7F,7F,3F);
+        Vector3 bossMinScale = new Vector3(10F,10F,5F);
         
         if (other.tag == "DestroyedEnemy")
         {
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float time = 10f;
     GameObject player;
+    [SerializeField] float rotationSpeed = 300;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,12 @@ public class Enemy : MonoBehaviour
         Vector3 playerPoition = player.transform.position;
         Vector3 firstPosition = gameObject.transform.position;
         Vector3 GoalPos = new Vector3(playerPoition.x,0,playerPoition.z);
-        transform.position = Vector3.Lerp(firstPosition, GoalPos, time * Time.deltaTime);   
+        transform.position = Vector3.Lerp(firstPosition, GoalPos, time * Time.deltaTime);
+
+        Vector3 movementDirection = playerPoition - firstPosition;
+
+        Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);   
     }
 }
